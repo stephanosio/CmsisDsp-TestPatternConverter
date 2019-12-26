b8b93596638c36f5cdf8219adfcb47ab14073422
Stephanos Ioannidis <root@stephanos.io>
2019-12-26 14:49:00 +0900

    Add 'distance' test models
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace TestPatternConverter
             StringWriter writer = new StringWriter();
             TestConverter tc = new TestConverter(TestCodeBasePath, PatternBasePath);
 
-            tc.Convert(writer, TransformTest.Find("transform/src/rf64.c"));
+            tc.Convert(writer, DistanceTest.Find("distance/src/f32.c"));
 
             Console.WriteLine(writer.ToString());
         }
---
+++ Tests/DistanceTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+using TestPatternConverter.Models;
+
+namespace TestPatternConverter.Tests
+{
+    public class DistanceTest : Test
+    {
+        public override IEnumerable<TestModel> Tests { get; } = new List<TestModel>
+        {
+            //
+            // Distance Tests
+            //
+
+            // U32
+            new TestModel
+            {
+                TestCodePath = "distance/src/u32.c",
+                PatternRootPath = "Distance/DistanceU32",
+                Patterns = new List<PatternModel>
+                {
+                    new PatternModel("uint32_t", "in_com1", "InputA1_u32.txt"),
+                    new PatternModel("uint32_t", "in_com2", "InputB1_u32.txt"),
+                    new PatternModel("uint16_t", "in_dims", "Dims1_s16.txt"),
+                    new PatternModel("uint32_t", "ref_dice", "Ref1_f32.txt"),
+                    new PatternModel("uint32_t", "ref_hamming", "Ref2_f32.txt"),
+                    new PatternModel("uint32_t", "ref_jaccard", "Ref3_f32.txt"),
+                    new PatternModel("uint32_t", "ref_kulsinski", "Ref4_f32.txt"),
+                    new PatternModel("uint32_t", "ref_rogerstanimoto", "Ref5_f32.txt"),
+                    new PatternModel("uint32_t", "ref_russellrao", "Ref6_f32.txt"),
+                    new PatternModel("uint32_t", "ref_sokalmichener", "Ref7_f32.txt"),
+                    new PatternModel("uint32_t", "ref_sokalsneath", "Ref8_f32.txt"),
+                    new PatternModel("uint32_t", "ref_yule", "Ref9_f32.txt")
+                }
+            },
+
+            // F32
+            new TestModel
+            {
+                TestCodePath = "distance/src/f32.c",
+                PatternRootPath = "Distance/DistanceF32",
+                Patterns = new List<PatternModel>
+                {
+                    new PatternModel("uint32_t", "in_com1", "InputA1_f32.txt"),
+                    new PatternModel("uint32_t", "in_com2", "InputB1_f32.txt"),
+                    new PatternModel("uint32_t", "in_jen1", "InputA8_f32.txt"),
+                    new PatternModel("uint32_t", "in_jen2", "InputB8_f32.txt"),
+                    new PatternModel("uint16_t", "in_dims", "Dims1_s16.txt"),
+                    new PatternModel("uint16_t", "in_dims_minkowski", "Dims9_s16.txt"),
+                    new PatternModel("uint32_t", "ref_braycurtis", "Ref1_f32.txt"),
+                    new PatternModel("uint32_t", "ref_canberra", "Ref2_f32.txt"),
+                    new PatternModel("uint32_t", "ref_chebyshev", "Ref3_f32.txt"),
+                    new PatternModel("uint32_t", "ref_cityblock", "Ref4_f32.txt"),
+                    new PatternModel("uint32_t", "ref_correlation", "Ref5_f32.txt"),
+                    new PatternModel("uint32_t", "ref_cosine", "Ref6_f32.txt"),
+                    new PatternModel("uint32_t", "ref_euclidean", "Ref7_f32.txt"),
+                    new PatternModel("uint32_t", "ref_jensenshannon", "Ref8_f32.txt"),
+                    new PatternModel("uint32_t", "ref_minkowski", "Ref9_f32.txt")
+                }
+            }
+        };
+    }
+}
