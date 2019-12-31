590aba9dcb3cdb4bdca3a4b16394269df063e5fc
Stephanos Ioannidis <root@stephanos.io>
2020-01-01 01:51:46 +0900

    Rename project name to 'CmsisTestPatternConverter'
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CmsisDsp-TestPatternConverter")]
+[assembly: AssemblyTitle("CMSIS Test Pattern Converter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CmsisDsp-TestPatternConverter")]
+[assembly: AssemblyProduct("CMSIS Test Pattern Converter")]
 [assembly: AssemblyCopyright("Copyright (c) 2020 Stephanos Ioannidis <root@stephanos.io>")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
