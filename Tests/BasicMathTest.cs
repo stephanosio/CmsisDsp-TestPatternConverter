9eec7c4c3ebd12a2f9778107b21b48d868a24800
Stephanos Ioannidis <root@stephanos.io>
2021-08-19 00:01:57 +0900

    Update 'BasicMath' test model for CMSIS-DSP 1.9.0
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- Tests/BasicMathTest.cs
+++ Tests/BasicMathTest.cs
@@ -34,6 +34,7 @@ namespace TestPatternConverter.Tests
                     new PatternModel("q7_t", "in_maxpos", "MaxPosInput12_s8.txt"),
                     new PatternModel("q7_t", "in_maxneg", "MaxNegInput12_s8.txt"),
                     new PatternModel("q7_t", "in_maxneg2", "MaxNeg2Input12_s8.txt"),
+                    new PatternModel("q7_t", "in_clip", "Input28_q7.txt"),
                     new PatternModel("q7_t", "ref_add", "Reference1_q7.txt"),
                     new PatternModel("q7_t", "ref_add_possat", "PosSat12_q7.txt"),
                     new PatternModel("q7_t", "ref_add_negsat", "NegSat13_q7.txt"),
@@ -52,14 +53,18 @@ namespace TestPatternConverter.Tests
                     new PatternModel("q31_t", "ref_dot_prod_3", "Reference7_q31.txt"),
                     new PatternModel("q31_t", "ref_dot_prod_4", "Reference8_q31.txt"),
                     new PatternModel("q31_t", "ref_dot_prod_4n1", "Reference9_q31.txt"),
+                    new PatternModel("q31_t", "ref_dot_prod_long", "Reference11_q31.txt"),
                     new PatternModel("q7_t", "ref_abs", "Reference10_q7.txt"),
                     new PatternModel("q7_t", "ref_shift", "Shift21_q7.txt"),
                     new PatternModel("q7_t", "ref_shift_possat", "Shift22_q7.txt"),
                     new PatternModel("q7_t", "ref_shift_negsat", "Shift23_q7.txt"),
-                    new PatternModel("u8_t", "ref_and", "And24_s8.txt"),
-                    new PatternModel("u8_t", "ref_or", "Or25_s8.txt"),
-                    new PatternModel("u8_t", "ref_not", "Not26_s8.txt"),
-                    new PatternModel("u8_t", "ref_xor", "Xor27_s8.txt")
+                    new PatternModel("uint8_t", "ref_and", "And24_s8.txt"),
+                    new PatternModel("uint8_t", "ref_or", "Or25_s8.txt"),
+                    new PatternModel("uint8_t", "ref_not", "Not26_s8.txt"),
+                    new PatternModel("uint8_t", "ref_xor", "Xor27_s8.txt"),
+                    new PatternModel("q7_t", "ref_clip1", "Reference28_q7.txt"),
+                    new PatternModel("q7_t", "ref_clip2", "Reference29_q7.txt"),
+                    new PatternModel("q7_t", "ref_clip3", "Reference30_q7.txt")
                 }
             },
 
@@ -78,6 +83,7 @@ namespace TestPatternConverter.Tests
                     new PatternModel("q15_t", "in_maxpos", "MaxPosInput12_s16.txt"),
                     new PatternModel("q15_t", "in_maxneg", "MaxNegInput12_s16.txt"),
                     new PatternModel("q15_t", "in_maxneg2", "MaxNeg2Input12_s16.txt"),
+                    new PatternModel("q15_t", "in_clip", "Input28_q15.txt"),
                     new PatternModel("q15_t", "ref_add", "Reference1_q15.txt"),
                     new PatternModel("q15_t", "ref_add_possat", "PosSat12_q15.txt"),
                     new PatternModel("q15_t", "ref_add_negsat", "NegSat13_q15.txt"),
@@ -96,14 +102,18 @@ namespace TestPatternConverter.Tests
                     new PatternModel("q63_t", "ref_dot_prod_3", "Reference7_q63.txt"),
                     new PatternModel("q63_t", "ref_dot_prod_4", "Reference8_q63.txt"),
                     new PatternModel("q63_t", "ref_dot_prod_4n1", "Reference9_q63.txt"),
+                    new PatternModel("q63_t", "ref_dot_prod_long", "Reference11_q63.txt"),
                     new PatternModel("q15_t", "ref_abs", "Reference10_q15.txt"),
                     new PatternModel("q15_t", "ref_shift", "Shift21_q15.txt"),
                     new PatternModel("q15_t", "ref_shift_possat", "Shift22_q15.txt"),
                     new PatternModel("q15_t", "ref_shift_negsat", "Shift23_q15.txt"),
-                    new PatternModel("u16_t", "ref_and", "And24_s16.txt"),
-                    new PatternModel("u16_t", "ref_or", "Or25_s16.txt"),
-                    new PatternModel("u16_t", "ref_not", "Not26_s16.txt"),
-                    new PatternModel("u16_t", "ref_xor", "Xor27_s16.txt")
+                    new PatternModel("uint16_t", "ref_and", "And24_s16.txt"),
+                    new PatternModel("uint16_t", "ref_or", "Or25_s16.txt"),
+                    new PatternModel("uint16_t", "ref_not", "Not26_s16.txt"),
+                    new PatternModel("uint16_t", "ref_xor", "Xor27_s16.txt"),
+                    new PatternModel("q15_t", "ref_clip1", "Reference28_q15.txt"),
+                    new PatternModel("q15_t", "ref_clip2", "Reference29_q15.txt"),
+                    new PatternModel("q15_t", "ref_clip3", "Reference30_q15.txt")
                 }
             },
 
@@ -122,6 +132,7 @@ namespace TestPatternConverter.Tests
                     new PatternModel("q31_t", "in_maxpos", "MaxPosInput12_s32.txt"),
                     new PatternModel("q31_t", "in_maxneg", "MaxNegInput12_s32.txt"),
                     new PatternModel("q31_t", "in_maxneg2", "MaxNeg2Input12_s32.txt"),
+                    new PatternModel("q31_t", "in_clip", "Input28_q31.txt"),
                     new PatternModel("q31_t", "ref_add", "Reference1_q31.txt"),
                     new PatternModel("q31_t", "ref_add_possat", "PosSat12_q31.txt"),
                     new PatternModel("q31_t", "ref_add_negsat", "NegSat13_q31.txt"),
@@ -140,14 +151,45 @@ namespace TestPatternConverter.Tests
                     new PatternModel("q63_t", "ref_dot_prod_3", "Reference7_q63.txt"),
                     new PatternModel("q63_t", "ref_dot_prod_4", "Reference8_q63.txt"),
                     new PatternModel("q63_t", "ref_dot_prod_4n1", "Reference9_q63.txt"),
+                    new PatternModel("q63_t", "ref_dot_prod_long", "Reference11_q63.txt"),
                     new PatternModel("q31_t", "ref_abs", "Reference10_q31.txt"),
                     new PatternModel("q31_t", "ref_shift", "Shift21_q31.txt"),
                     new PatternModel("q31_t", "ref_shift_possat", "Shift22_q31.txt"),
                     new PatternModel("q31_t", "ref_shift_negsat", "Shift23_q31.txt"),
-                    new PatternModel("u32_t", "ref_and", "And24_s32.txt"),
-                    new PatternModel("u32_t", "ref_or", "Or25_s32.txt"),
-                    new PatternModel("u32_t", "ref_not", "Not26_s32.txt"),
-                    new PatternModel("u32_t", "ref_xor", "Xor27_s32.txt")
+                    new PatternModel("uint32_t", "ref_and", "And24_s32.txt"),
+                    new PatternModel("uint32_t", "ref_or", "Or25_s32.txt"),
+                    new PatternModel("uint32_t", "ref_not", "Not26_s32.txt"),
+                    new PatternModel("uint32_t", "ref_xor", "Xor27_s32.txt"),
+                    new PatternModel("q31_t", "ref_clip1", "Reference28_q31.txt"),
+                    new PatternModel("q31_t", "ref_clip2", "Reference29_q31.txt"),
+                    new PatternModel("q31_t", "ref_clip3", "Reference30_q31.txt")
+                }
+            },
+
+            // F16
+            new TestModel
+            {
+                TestCodePath = "basicmath/src/f16.c",
+                PatternRootPath = "BasicMaths/BasicMathsF16",
+                Patterns = new List<PatternModel>
+                {
+                    new PatternModel("uint16_t", "in_com1", "Input1_f16.txt"),
+                    new PatternModel("uint16_t", "in_com2", "Input2_f16.txt"),
+                    new PatternModel("uint16_t", "in_clip", "Input12_f16.txt"),
+                    new PatternModel("uint16_t", "ref_add", "Reference1_f16.txt"),
+                    new PatternModel("uint16_t", "ref_sub", "Reference2_f16.txt"),
+                    new PatternModel("uint16_t", "ref_mult", "Reference3_f16.txt"),
+                    new PatternModel("uint16_t", "ref_negate", "Reference4_f16.txt"),
+                    new PatternModel("uint16_t", "ref_offset", "Reference5_f16.txt"),
+                    new PatternModel("uint16_t", "ref_scale", "Reference6_f16.txt"),
+                    new PatternModel("uint16_t", "ref_dot_prod_3", "Reference7_f16.txt"),
+                    new PatternModel("uint16_t", "ref_dot_prod_4", "Reference8_f16.txt"),
+                    new PatternModel("uint16_t", "ref_dot_prod_4n1", "Reference9_f16.txt"),
+                    new PatternModel("uint16_t", "ref_dot_prod_long", "Reference11_f16.txt"),
+                    new PatternModel("uint16_t", "ref_abs", "Reference10_f16.txt"),
+                    new PatternModel("uint16_t", "ref_clip1", "Reference12_f16.txt"),
+                    new PatternModel("uint16_t", "ref_clip2", "Reference13_f16.txt"),
+                    new PatternModel("uint16_t", "ref_clip3", "Reference14_f16.txt")
                 }
             },
 
@@ -160,6 +202,7 @@ namespace TestPatternConverter.Tests
                 {
                     new PatternModel("uint32_t", "in_com1", "Input1_f32.txt"),
                     new PatternModel("uint32_t", "in_com2", "Input2_f32.txt"),
+                    new PatternModel("uint32_t", "in_clip", "Input12_f32.txt"),
                     new PatternModel("uint32_t", "ref_add", "Reference1_f32.txt"),
                     new PatternModel("uint32_t", "ref_sub", "Reference2_f32.txt"),
                     new PatternModel("uint32_t", "ref_mult", "Reference3_f32.txt"),
@@ -169,7 +212,11 @@ namespace TestPatternConverter.Tests
                     new PatternModel("uint32_t", "ref_dot_prod_3", "Reference7_f32.txt"),
                     new PatternModel("uint32_t", "ref_dot_prod_4", "Reference8_f32.txt"),
                     new PatternModel("uint32_t", "ref_dot_prod_4n1", "Reference9_f32.txt"),
-                    new PatternModel("uint32_t", "ref_abs", "Reference10_f32.txt")
+                    new PatternModel("uint32_t", "ref_dot_prod_long", "Reference11_f32.txt"),
+                    new PatternModel("uint32_t", "ref_abs", "Reference10_f32.txt"),
+                    new PatternModel("uint32_t", "ref_clip1", "Reference12_f32.txt"),
+                    new PatternModel("uint32_t", "ref_clip2", "Reference13_f32.txt"),
+                    new PatternModel("uint32_t", "ref_clip3", "Reference14_f32.txt")
                 }
             }
         };
