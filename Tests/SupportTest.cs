9aa7e9557895a07fb4d6f83a5d613110aed2cd19
Stephanos Ioannidis <root@stephanos.io>
2021-08-22 00:32:58 +0900

    Update 'Support' test model for CMSIS-DSP 1.9.0
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- Tests/SupportTest.cs
+++ Tests/SupportTest.cs
@@ -61,6 +61,22 @@ namespace TestPatternConverter.Tests
                 }
             },
 
+            // F16
+            new TestModel
+            {
+                TestCodePath = "support/src/f16.c",
+                PatternRootPath = "Support/SupportF16",
+                Patterns = new List<PatternModel>
+                {
+                    new PatternModel("uint16_t", "in_weighted_sum_val", "Inputs6_f16.txt"),
+                    new PatternModel("uint16_t", "in_weighted_sum_coeff", "Weights6_f16.txt"),
+                    new PatternModel("uint32_t", "ref_f32", "Samples1_f32.txt"),
+                    new PatternModel("q15_t", "ref_q15", "Samples3_q15.txt"),
+                    new PatternModel("uint16_t", "ref_f16", "Samples11_f16.txt"),
+                    new PatternModel("uint16_t", "ref_weighted_sum", "Ref6_f16.txt")
+                }
+            },
+
             // F32
             new TestModel
             {
@@ -75,15 +91,6 @@ namespace TestPatternConverter.Tests
                     new PatternModel("uint32_t", "in_sort_bitonic_16", "Input8_f32.txt"),
                     new PatternModel("uint32_t", "in_sort_bitonic_32", "Input9_f32.txt"),
                     new PatternModel("uint32_t", "in_sort_const", "Input10_f32.txt"),
-                    new PatternModel("uint32_t", "in_spline_squ_x", "InputX11_f32.txt"),
-                    new PatternModel("uint32_t", "in_spline_squ_y", "InputY11_f32.txt"),
-                    new PatternModel("uint32_t", "in_spline_squ_xq", "OutputX11_f32.txt"),
-                    new PatternModel("uint32_t", "in_spline_sin_x", "InputX12_f32.txt"),
-                    new PatternModel("uint32_t", "in_spline_sin_y", "InputY12_f32.txt"),
-                    new PatternModel("uint32_t", "in_spline_sin_xq", "OutputX12_f32.txt"),
-                    new PatternModel("uint32_t", "in_spline_ram_x", "InputX13_f32.txt"),
-                    new PatternModel("uint32_t", "in_spline_ram_y", "InputY13_f32.txt"),
-                    new PatternModel("uint32_t", "in_spline_ram_xq", "OutputX13_f32.txt"),
                     new PatternModel("q31_t", "ref_q31", "Samples4_q31.txt"),
                     new PatternModel("q15_t", "ref_q15", "Samples3_q15.txt"),
                     new PatternModel("q7_t", "ref_q7", "Samples5_q7.txt"),
@@ -91,10 +98,21 @@ namespace TestPatternConverter.Tests
                     new PatternModel("uint32_t", "ref_sort", "Reference7_f32.txt"),
                     new PatternModel("uint32_t", "ref_sort_bitonic_16", "Reference8_f32.txt"),
                     new PatternModel("uint32_t", "ref_sort_bitonic_32", "Reference9_f32.txt"),
-                    new PatternModel("uint32_t", "ref_sort_const", "Reference10_f32.txt"),
-                    new PatternModel("uint32_t", "ref_spline_squ", "Reference11_f32.txt"),
-                    new PatternModel("uint32_t", "ref_spline_sin", "Reference12_f32.txt"),
-                    new PatternModel("uint32_t", "ref_spline_ram", "Reference13_f32.txt")
+                    new PatternModel("uint32_t", "ref_sort_const", "Reference10_f32.txt")
+                }
+            },
+
+            // F16 Barycenter
+            new TestModel
+            {
+                TestCodePath = "support/src/barycenter_f16.c",
+                PatternRootPath = "SupportBarF16",
+                Patterns = new List<PatternModel>
+                {
+                    new PatternModel("uint16_t", "in_barycenter_val", "Inputs1_f16.txt"),
+                    new PatternModel("uint16_t", "in_barycenter_coeff", "Weights1_f16.txt"),
+                    new PatternModel("uint16_t", "in_barycenter_dims", "Dims1_s16.txt"),
+                    new PatternModel("uint16_t", "ref_barycenter", "Ref1_f16.txt")
                 }
             },
 
