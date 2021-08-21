91a8ad1ca95ffc175ef974e73cbab6d3e1d8f9a2
Stephanos Ioannidis <root@stephanos.io>
2021-08-22 01:29:01 +0900

    Update 'SVM' test model for CMSIS-DSP 1.9.0
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- Tests/SvmTest.cs
+++ Tests/SvmTest.cs
@@ -19,6 +19,40 @@ namespace TestPatternConverter.Tests
             // SVM Tests
             //
 
+            // F16
+            new TestModel
+            {
+                TestCodePath = "svm/src/f16.c",
+                PatternRootPath = "SVM/SVMF16",
+                Patterns = new List<PatternModel>
+                {
+                    new PatternModel("uint16_t", "in_linear_val", "Samples1_f16.txt"),
+                    new PatternModel("uint16_t", "in_linear_param", "Params1_f16.txt"),
+                    new PatternModel("uint16_t", "in_linear_dims", "Dims1_s16.txt"),
+                    new PatternModel("uint32_t", "ref_linear", "Reference1_s32.txt"),
+
+                    new PatternModel("uint16_t", "in_polynomial_val", "Samples2_f16.txt"),
+                    new PatternModel("uint16_t", "in_polynomial_param", "Params2_f16.txt"),
+                    new PatternModel("uint16_t", "in_polynomial_dims", "Dims2_s16.txt"),
+                    new PatternModel("uint32_t", "ref_polynomial", "Reference2_s32.txt"),
+
+                    new PatternModel("uint16_t", "in_rbf_val", "Samples3_f16.txt"),
+                    new PatternModel("uint16_t", "in_rbf_param", "Params3_f16.txt"),
+                    new PatternModel("uint16_t", "in_rbf_dims", "Dims3_s16.txt"),
+                    new PatternModel("uint32_t", "ref_rbf", "Reference3_s32.txt"),
+
+                    new PatternModel("uint16_t", "in_sigmoid_val", "Samples4_f16.txt"),
+                    new PatternModel("uint16_t", "in_sigmoid_param", "Params4_f16.txt"),
+                    new PatternModel("uint16_t", "in_sigmoid_dims", "Dims4_s16.txt"),
+                    new PatternModel("uint32_t", "ref_sigmoid", "Reference4_s32.txt"),
+
+                    new PatternModel("uint16_t", "in_oneclass_val", "Samples5_f16.txt"),
+                    new PatternModel("uint16_t", "in_oneclass_param", "Params5_f16.txt"),
+                    new PatternModel("uint16_t", "in_oneclass_dims", "Dims5_s16.txt"),
+                    new PatternModel("uint32_t", "ref_oneclass", "Reference5_s32.txt")
+                }
+            },
+
             // F32
             new TestModel
             {
